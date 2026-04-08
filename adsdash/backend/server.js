ec91f9b8e351f61a9ea58406dcf4bcce6c06f876
antonella-cmd <antonella@pticonsultingpartner.com>
2026-04-07 21:28:03 -0300

    Update server.js

--- adsdash/backend/server.js
+++ adsdash/backend/server.js
@@ -30,7 +30,11 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: process.env.NODE_ENV === 'production', maxAge: 24 * 60 * 60 * 1000 },
+  cookie: { 
+    secure: true,
+    sameSite: 'none',
+    maxAge: 24 * 60 * 60 * 1000
+  },
 }));
 
 app.use('/api/auth',      authRoutes);
